ba305d2243ec9cae456cd498c19b0c004f18b707
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-02-25 18:04:14 +0700

    update hak akses

--- routes/index.js
+++ routes/index.js
@@ -17,65 +17,38 @@ router.get("/fetch", verifyToken, AuthController.Fetch);
 router.post(
   "/medicine",
   verifyToken,
-  IsAdmin,
+
   MedicineController.createMedicine
 );
-router.get("/medicine", verifyToken, IsAdmin, MedicineController.getMedicine);
-router.get("/medicine/:id", verifyToken, IsAdmin, MedicineController.getDetailMedicine);
-router.put(
-  "/medicine/:id",
-  verifyToken,
-  IsAdmin,
-  MedicineController.updatetMedicine
-);
-router.delete(
-  "/medicine/:id",
-  verifyToken,
-  IsAdmin,
-  MedicineController.deleteMedicine
-);
+router.get("/medicine", verifyToken, MedicineController.getMedicine);
+router.get("/medicine/:id", verifyToken, MedicineController.getDetailMedicine);
+router.put("/medicine/:id", verifyToken, MedicineController.updatetMedicine);
+router.delete("/medicine/:id", verifyToken, MedicineController.deleteMedicine);
 
 //patient
-router.post("/patient", verifyToken, IsAdmin, PatientController.createPatient);
-router.get("/patient", verifyToken, IsAdmin, PatientController.getPatient);
-router.get("/patient/:id", verifyToken, IsAdmin, PatientController.detailPatient);
-router.put("/patient/:id", verifyToken, IsAdmin, PatientController.updatePatient);
-router.delete("/patient/:id", verifyToken, IsAdmin, PatientController.deletePatient);
+router.post("/patient", verifyToken, PatientController.createPatient);
+router.get("/patient", verifyToken, PatientController.getPatient);
+router.get("/patient/:id", verifyToken, PatientController.detailPatient);
+router.put("/patient/:id", verifyToken, PatientController.updatePatient);
+router.delete("/patient/:id", verifyToken, PatientController.deletePatient);
 
 //Rekam Medis
-router.post(
-  "/rekam-medis",
-  verifyToken,
-  IsAdmin,
-  RekamMedisController.createRekamMedis
-);
-router.get("/rekam-medis", verifyToken, IsAdmin, RekamMedisController.getRM);
-router.get(
-  "/rekam-medis/:id",
-  verifyToken,
-  IsAdmin,
-  RekamMedisController.getDetailRM
-);
+router.post("/rekam-medis", verifyToken, RekamMedisController.createRekamMedis);
+router.get("/rekam-medis", verifyToken, RekamMedisController.getRM);
+router.get("/rekam-medis/:id", verifyToken, RekamMedisController.getDetailRM);
 router.get(
   "/rekam-medis/patient/:id",
   verifyToken,
-  IsAdmin,
   RekamMedisController.getDetailbyPatient
 );
 router.delete(
   "/rekam-medis/:id",
   verifyToken,
-  IsAdmin,
   RekamMedisController.deleteRekamMedis
 );
 
 //Invoice
-router.get("/invoice", verifyToken, IsAdmin, TransactionController.getInvoice);
-router.put(
-  "/invoice/:id",
-  verifyToken,
-  IsAdmin,
-  TransactionController.updateInvoice
-);
+router.get("/invoice", verifyToken, TransactionController.getInvoice);
+router.put("/invoice/:id", verifyToken, TransactionController.updateInvoice);
 
 module.exports = router;
